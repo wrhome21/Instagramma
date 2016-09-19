4e94ab3a606642f7a173ea89a1be12443f01f8b1
nematode195 <mike.tallsmall@gmail.com>
2016-09-19 10:35:43 -0400

    server using bodyparser

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 var app = express();
+var bodyParser = require('body-parser')
+var db = require('./db.js'); 
+
+
+
+
+app.use(bodyParser.json())
+
 
 var compiler = webpack(webpackConfig);
-
+ 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
   publicPath: '/',
-  stats: {
+  stats: { 
     colors: true,
   },
   historyApiFallback: true,
@@ -19,9 +29,22 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(express.static(__dirname + '/www'));
 
 app.post('/user', function (req, res) {
-    
-	console.log("Entry: /user ");
-	res.send(JSON.stringify("user posted"));
+	//db.initDB(db); this happens when requiring
+    //res.setHeader('Content-Type', 'text/plain')
+  console.log("app.Post");
+  console.log(req.body);
+  console.log("req.body.username " + req.body.userName );
+  var userName = req.body.userName;
+ var p = db.insertUserProfile("", userName, "", ""); 
+     p.then((val) => { 
+         console.log("PK: " + val); 
+         aUser.primaryKey=val; 
+         printUser(aUser); 
+     }).catch((err) => {console.log(err);}); 
+
+  //res.send('how do i get the req value from this ajax post????');
+  res.end(JSON.stringify(req.body))
+
 });
 
 var server = app.listen(3000, function() {
